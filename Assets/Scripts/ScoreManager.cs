3c2cdea7a210937d1cd585b223bb48120934ff88
Connor Kerwin <connorkerwin@yahoo.co.uk>
2018-11-11 14:29:50 +0000

    Rearranged source files and added a manager to test functionality.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LightweightManagementSystem;
+
+public class ScoreManager : MonoManager
+{
+    [SerializeField]
+    private int score;
+
+    public override void OnManagerRegistered(CoreBehaviour coreBehaviour)
+    {
+
+    }
+
+    public override void OnManagerUnregistered()
+    {
+
+    }
+
+    public void AddScore(int score)
+    {
+        this.score += score;
+    }
+}
---
+++ Assets/Scripts/ScoreTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LightweightManagementSystem;
+
+public class ScoreTest : MonoBehaviour
+{
+    [SerializeField]
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.R))
+        {
+            CoreBehaviour cb = CoreBehaviour.Instance;
+            ScoreManager scoreManager = cb.GetFirst<ScoreManager>();
+            scoreManager.AddScore(score);
+        }
+	}
+}
